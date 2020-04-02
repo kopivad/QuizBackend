d548faf03e069c6c89b2d23c48fc548b9378db41
Vadym <kopivad@gmail.com>
2020-04-02 13:59:01 +0300

    Hibernate ApiClientDao and UserDao impl

---
+++ src/main/java/com/kopivad/quizzes/repository/hibernate/ApiClientRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.kopivad.quizzes.repository.hibernate;
+
+import com.kopivad.quizzes.domain.api.ApiClient;
+import com.kopivad.quizzes.repository.ApiClientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository("hibernateApiClientRepository")
+@AllArgsConstructor
+public class ApiClientRepositoryImpl implements ApiClientRepository {
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Override
+    public ApiClient findByUsername(String username) {
+        return entityManager
+                .createQuery("select c from ApiClient c where username = :username", ApiClient.class)
+                .setParameter("username", username)
+                .getSingleResult();
+    }
+}
