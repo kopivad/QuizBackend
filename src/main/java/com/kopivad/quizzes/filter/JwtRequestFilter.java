dd8c24cfd9184b6050ffff1fe0268730e27128d4
Vadym <kopivad@gmail.com>
2020-04-09 12:45:09 +0300

    Filter refactor

--- src/main/java/com/kopivad/quizzes/filter/JwtRequestFilter.java
+++ src/main/java/com/kopivad/quizzes/filter/JwtRequestFilter.java
@@ -24,16 +24,9 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        final String authorizationHeader = httpServletRequest.getHeader("Authorization");
-
-        String username = null;
-        String jwt = null;
-
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            jwt = authorizationHeader.substring(7);
-            username = jwtService.extractUsername(jwt);
-        }
-
+        String authorizationHeader = httpServletRequest.getHeader("Authorization");
+        String jwt = authorizationHeader.substring(7);
+        String username = jwtService.extractUsername(jwt);
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
