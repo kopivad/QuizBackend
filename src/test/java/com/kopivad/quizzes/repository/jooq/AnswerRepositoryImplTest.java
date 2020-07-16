b02419be72bda5deca1ca45a6e4de29644f5eaa7
Vadym <vadym.kopytsia@gmail.com>
2020-07-16 12:11:28 +0300

    Repository unit tests refactoring

--- src/test/java/com/kopivad/quizzes/repository/jooq/AnswerRepositoryImplTest.java
+++ src/test/java/com/kopivad/quizzes/repository/jooq/AnswerRepositoryImplTest.java
@@ -13,84 +13,65 @@ import org.junit.Test;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class AnswerRepositoryImplTest {
-    private static DSLContext dslContext;
     private static AnswerRepository answerRepository;
 
 
     @BeforeClass
     public static void init() {
-        dslContext = DSL.using(TestUtils.createTestDefaultPgDataSource(), SQLDialect.POSTGRES);
+        DSLContext dslContext = DSL.using(TestUtils.createTestDefaultPgDataSource(), SQLDialect.POSTGRES);
         answerRepository = new AnswerRepositoryImpl(dslContext);
     }
 
     @Test
     public void findAllTest() {
         List<Answer> generatedAnswers = AnswerUtils.generateAnswers(5);
-        List<Answer> savedAnswers = generatedAnswers
+        List<Answer> expected = generatedAnswers
                 .stream()
-                .map(answer -> answerRepository.save(answer))
+                .map(answer -> answer.toBuilder().id(answerRepository.save(answer)).build())
                 .collect(Collectors.toUnmodifiableList());
-        List<Answer> allAnswers = answerRepository.findAll();
+        List<Answer> actual = answerRepository.findAll();
 
-        assertThat(savedAnswers, notNullValue());
-        assertThat(allAnswers, notNullValue());
-        assertTrue(allAnswers.containsAll(savedAnswers));
+        assertTrue(actual.containsAll(expected));
     }
 
     @Test
     public void findByIdTest() {
         Answer generatedAnswer = AnswerUtils.generateAnswer();
-        Answer savedAnswer = answerRepository.save(generatedAnswer);
-        Answer answer = answerRepository.findById(savedAnswer.getId());
+        long expected = answerRepository.save(generatedAnswer);
+        Answer actual = answerRepository.findById(expected);
 
-        assertThat(savedAnswer, notNullValue());
-        assertThat(answer, notNullValue());
-        assertEquals(savedAnswer, answer);
+        assertThat(actual.getId(), is(expected));
     }
 
     @Test
     public void saveTest() {
         Answer generatedAnswer = AnswerUtils.generateAnswer();
-        int accountsCountBeforeInsert = answerRepository.findAll().size();
-        Answer answer = answerRepository.save(generatedAnswer);
-        List<Answer> allAnswers = answerRepository.findAll();
+        long actual = answerRepository.save(generatedAnswer);
 
-        assertThat(answer.getId(), notNullValue());
-        assertThat(accountsCountBeforeInsert + 1, equalTo(allAnswers.size()));
-        assertThat(allAnswers, hasItem(answer));
+        assertThat(actual, notNullValue());
     }
 
     @Test
     public void updateTest() {
-        String dataForUpdate = "Some text";
         Answer generatedAnswer = AnswerUtils.generateAnswer();
-        Answer savedAnswer = answerRepository.save(generatedAnswer);
-        Answer answerWithText = generatedAnswer.toBuilder().body(dataForUpdate).build();
-        Answer updatedUser = answerRepository.update(savedAnswer.getId(), answerWithText);
+        Answer savedAnswer = generatedAnswer.toBuilder().id(answerRepository.save(generatedAnswer)).build();
+        boolean actual = answerRepository.update(savedAnswer);
 
-
-        assertThat(savedAnswer, notNullValue());
-        assertThat(updatedUser, notNullValue());
-        assertThat(updatedUser.getId(), equalTo(updatedUser.getId()));
-        assertThat(savedAnswer.getBody(), not(equalTo(updatedUser.getBody())));
+        assertTrue(actual);
     }
 
     @Test
     public void deleteTest() {
         Answer generatedAnswer = AnswerUtils.generateAnswer();
-        Answer savedAnswer = answerRepository.save(generatedAnswer);
-        List<Answer> allAnswersBeforeDeleting = answerRepository.findAll();
-        answerRepository.delete(savedAnswer.getId());
-        List<Answer> allAnswersAfterDeleting = answerRepository.findAll();
+        long id = answerRepository.save(generatedAnswer);
+        boolean actual = answerRepository.delete(id);
 
-        assertThat(savedAnswer, notNullValue());
-        assertThat(allAnswersBeforeDeleting, notNullValue());
-        assertThat(allAnswersAfterDeleting, notNullValue());
-        assertThat(allAnswersBeforeDeleting, hasItem(savedAnswer));
-        assertThat(allAnswersAfterDeleting, not(hasItem(savedAnswer)));
+        assertTrue(actual);
     }
 }
